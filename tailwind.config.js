3b11857979472e220be3b21b7a9bea1cb01c5fd9
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-08-21 19:19:16 -0300

    tailwind custom colors and new bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "primary-1": "#d9ed92",
+      "primary-2": "#b5e48c",
+      "primary-3": "#99d98c",
+      "primary-4": "#76c893",
+      "primary-5": "#52b69a",
+      "secondary-1": "#34a0a4",
+      "secondary-2": "#168aad",
+      "secondary-3": "#1a759f",
+      "secondary-4": "#1e6091",
+      "secondary-5": "#184e77",
+    },
     extend: {},
   },
   plugins: [],
